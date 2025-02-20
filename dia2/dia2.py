bd3b9e970af60016fc9b9a175ee5e4b743324f20
contato.geovanigaldino@gmail.com <164828104+geovanigaldino@users.noreply.github.com>
2025-02-20 12:44:18 -0300

    dia 2

---
+++ dia2/dia2.py
@@ -0,0 +1,6 @@
+primeiroNumero = float(input("Digite o primeiro número: "))
+segundoNumero = float(input("Digite o segundo número: "))
+
+resultado = primeiroNumero + segundoNumero
+
+print(f"A soma dos números {primeiroNumero} e {segundoNumero} é igual a {resultado}")
